d4ef69d9b35bcbe471cfb7e98677443e0753df3f
노동경 <dandy.noh@samsung.com>
2024-03-29 23:02:12 +0900

    기지국 설치 문제 추가
    Signed-off-by: 노동경 <dandy.noh@samsung.com>

--- src/day4/블록쌓기게임/UserSolution.java
+++ src/day4/블록쌓기게임/UserSolution.java
@@ -1,8 +1,21 @@
 package day4.블록쌓기게임;
 
 class UserSolution {
-
+	static int C;
+	static long total;
+	int[] tree, minTree, maxTree;
+	int[] lazy;
+	
+	static final int MOD = 1_000_000;
+	
     void init(int C) {
+    	this.C = C;
+    	total = 0;
+    	
+    	tree = new int[C * 4];
+    	minTree = new int[C * 4];
+    	maxTree = new int[C * 4];
+    	lazy = new int[C * 4];
     }
 
     Solution.Result dropBlocks(int mCol, int mHeight, int mLength) {
--- src/day5/기지국설치/UserSolution.java
+++ src/day5/기지국설치/UserSolution.java
@@ -1,19 +1,94 @@
 package day5.기지국설치;
 
+import java.util.HashMap;
+import java.util.TreeSet;
+
 class UserSolution {
+	static class Node implements Comparable<Node> {
+		int id;
+		int loc;
+		
+		Node (int id, int loc) {
+			this.id = id;
+			this.loc = loc;
+		}
+		
+		@Override
+		public int compareTo(Node o) {
+			return Integer.compare(this.loc, o.loc);
+		}
+	}
+	
+	static HashMap<Integer, Node> nodeMap = new HashMap<>();
+	static TreeSet<Node> nodeSet = new TreeSet<>();
+	
 	public void init(int N, int mId[], int mLocation[]) {
+		nodeMap.clear();
+		nodeSet.clear();
+		
+		Node node;
+		for (int i = 0; i < N; i++) {
+			node = new Node(mId[i], mLocation[i]);
+			nodeMap.put(mId[i], node);
+			nodeSet.add(node);
+		}
+		
 		return;
 	}
 
 	public int add(int mId, int mLocation) {
-		return 0;
+		if (nodeMap.containsKey(mId)) {
+			nodeSet.remove(nodeMap.get(mId));
+		}
+		Node node = new Node(mId, mLocation);
+		nodeMap.put(mId, node);
+		nodeSet.add(node);
+		
+		return nodeMap.size();
 	}
 
 	public int remove(int mStart, int mEnd) {
-		return 0;
+		Node node = new Node(0, mStart);
+		Node temp;
+		while (true) {
+			temp = nodeSet.higher(node);
+			if (temp == null || temp.loc > mEnd) {
+				break;
+			}
+			nodeSet.remove(temp);
+			nodeMap.remove(temp.id);
+		}
+		
+		return nodeMap.size();
 	}
 
 	int install(int M) {
-		return 0;
+		int start = 1;
+		int end = nodeSet.last().loc;
+		
+		int answer = 0;
+		int mid, first, cur, installCnt;
+		while (start <= end) {
+			mid = (start + end) / 2;
+			
+			installCnt = 1;
+			first = nodeSet.first().loc;
+			for (Node node : nodeSet) {
+				cur = node.loc;
+				if (cur - first >= mid) {
+					++installCnt;
+					first = cur;
+				}
+			}
+			
+			if (installCnt < M) {
+				end = mid - 1;
+			} else {
+				answer = Math.max(answer, mid);
+				start = mid + 1;
+			}
+		}
+		
+		return answer;
 	}
 }
